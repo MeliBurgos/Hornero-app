c0e868d73a8e9e120e666f9698918cc6b3d6817f
Fabian <fabianlopez1988@gmail.com>
2022-06-23 18:27:46 -0300

    commit Jueves

---
+++ api/routes/users.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/Users");
+
+router.post("/register", (req, res) => {
+  User.create(req.body)
+    .then((user) => {
+      res.send(user).status(201);
+    })
+    .catch((err) => console.log(err));
+});
+
+router.get("/", (req, res) => {
+    User.find({})
+    .then((users) => {
+        res.send(users).status(200);
+    })
+    .catch((err) => console.log(err));
+})
+
+
+module.exports = router;
