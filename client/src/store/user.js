a7e31247395a7f8d22d3bf7e532a0828549179d3
pablobarreiro <pbarreiro92@gmail.com>
2022-06-24 12:32:03 -0300

    elimino package.json extra

--- client/src/store/user.js
+++ client/src/store/user.js
@@ -2,12 +2,12 @@ import { createAsyncThunk, createReducer } from '@reduxjs/toolkit'
 import axios from 'axios'
 
 export const userRegister = createAsyncThunk("USER_REGISTER", (data) => {
-    return axios.post("http://localhost:3001/api/users/register", data)
+    return axios.post("/api/users/register", data)
         .then(user => user.data)
 });
 
 export const getUser = createAsyncThunk("GET_USER", () => {
-    return axios.get(`http://localhost:3001/api/users/62b5cb24f3eb5cd956bdc6ae`)
+    return axios.get(`/api/users/62b5cb24f3eb5cd956bdc6ae`)
         .then(user => user.data)
 
 })
