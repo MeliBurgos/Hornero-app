65a8721a556e5d31fa2f235fcfb0f10df56ea669
MatiasNasif <matiasezequielnasif@gmail.com>
2022-06-24 12:15:19 -0300

    redux in register and profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -4,28 +4,41 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import Image from 'react-bootstrap/Image'
 import { FaUserFriends, FaStar, FaPencilAlt } from "react-icons/fa"
 import {useNavigate} from "react-router-dom"
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { getUser } from "../store/user";
 
 import Friends from '../commons/Friends'
 import Favorites from '../commons/Favorites';
 
-const Profile = ({user}) => {
+const Profile = () => {
   const [showFriends, setShowFriends] = useState(false)
   const [showFavs, setShowFavs] = useState(false)
 
   let Navigate = useNavigate()
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user)
+  
+  useEffect(() => {
+    dispatch(getUser())
+      .then((res) => user = res)
+  }, [])
+ 
+
+
+
 
   return (
     <div className="text-center mt-3">
-      <Image  style={{ width: "60%", height: 'auto', maxWidth: "400px" }} roundedCircle="true" thumbnail="true" src={user.photo} />
+      <Image  style={{ width: "60%", height: 'auto', maxWidth: "400px" }} roundedCircle="true" thumbnail="true" src={user.imgUrl} />
 
       <Card.Body>
         <Card.Title>{user.name} {user.surname}</Card.Title>
       </Card.Body>
       <ListGroup>
         <ListGroup.Item>{user.email}</ListGroup.Item>
-        <ListGroup.Item>{user.mainofice}</ListGroup.Item>
-        <ListGroup.Item>{user.charge}</ListGroup.Item>
+        <ListGroup.Item>{user.mainOffice}</ListGroup.Item>
+        <ListGroup.Item>{user.position}</ListGroup.Item>
       <ListGroup.Item>
         <dt style={{ paddingTop:"5px" }}>
           <button style={{ maxWidth: "400px" }} onClick={()=>setShowFriends(true)} className="main-button"> <FaUserFriends /> Amigos </button>
