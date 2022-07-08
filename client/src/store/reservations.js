a1324bec75457ba85ec4f5f31ecd5ea3a7eec1f3
MatiasNasif <matiasezequielnasif@gmail.com>
2022-07-08 10:26:45 -0300

    modal reservas

--- client/src/store/reservations.js
+++ client/src/store/reservations.js
@@ -7,7 +7,13 @@ export const getReservations = createAsyncThunk("GET_RESERVATIONS", (id) => {
         .then(res => res.data)
 })
 
-export const getUserReservations = createAsyncThunk("GET_USER_RESERVATIONS", () => {
+export const getUserReservationsFuturas = createAsyncThunk("GET_USER_RESERVATIONS_FUTURAS", () => {
+    const userId = JSON.parse(localStorage.getItem('user')).user._id
+    return axios.get(`/api/reservations/users/date/${userId}/`)
+        .then(res => res.data)
+})
+
+export const getUserReservationsAnteriores = createAsyncThunk("GET_USER_RESERVATIONS_ANTERIORES", () => {
     const userId = JSON.parse(localStorage.getItem('user')).user._id
     return axios.get(`/api/reservations/users/${userId}/date`)
         .then(res => res.data)
@@ -15,7 +21,8 @@ export const getUserReservations = createAsyncThunk("GET_USER_RESERVATIONS", () 
 
 const ReservationReducer = createReducer({}, {
     [getReservations.fulfilled]: (state, action) => action.payload,
-    [getUserReservations.fulfilled]: (state, action) => action.payload
+    [getUserReservationsFuturas.fulfilled]: (state, action) => action.payload,
+    [getUserReservationsAnteriores.fulfilled]: (state, action) => action.payload
 });
 
 export default ReservationReducer;
