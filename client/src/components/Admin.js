f3010a329f4e0a2c166b3551ab3ec04125817df3
Fabian <fabianlopez1988@gmail.com>
2022-07-13 19:42:01 -0300

    fix

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -2,15 +2,12 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Table from "react-bootstrap/Table";
 import { FaUserLock, FaUser } from "react-icons/fa";
+
 import ProfileModal from "./ProfileModal";
-import {
-  promoteUserToAdmin,
-  removeUserToAdmin,
-  getAllUsers,
-} from "../store/admin";
+import { promoteUserToAdmin, removeUserToAdmin, getAllUsers } from "../store/admin";
 import useInput from "../hooks/useInput";
 
-const Admin = () => {
+const Admin = ({ setShow, setAddFriend }) => {
   const dispatch = useDispatch();
   const searchUsers = useInput();
   const [filteredUsers, setFilteredUsers] = useState([]);
@@ -31,23 +28,23 @@ const Admin = () => {
     });
   };
 
-  useEffect(() => {
-    dispatch(getAllUsers());
-  }, []);
+  useEffect(()=> {
+    dispatch(getAllUsers())
+  }, [])
 
   // buscar usuarios para agregar
   useEffect(() => {
     if (searchUsers.value.length >= 2) {
-      let newUsersList = [];
+      let newUsersList = []
       let searchLowerCase = searchUsers.value.toLowerCase();
       allUsers.forEach((user) => {
-        let fullName = `${user.name} ${user.surname}`;
-        return fullName.toLowerCase().includes(searchLowerCase) ||
-          user.mainOffice.toLowerCase().includes(searchLowerCase)
-          ? newUsersList.push(user)
-          : null;
-      });
-      setFilteredUsers(newUsersList);
+      let fullName = `${user.name} ${user.surname}`
+        return fullName.toLowerCase().includes(searchLowerCase)
+        || 
+        user.mainOffice.toLowerCase().includes(searchLowerCase) 
+        ? newUsersList.push(user) : null}
+      )
+      setFilteredUsers(newUsersList)
     }
   }, [searchUsers.value, allUsers]);
 
@@ -56,32 +53,31 @@ const Admin = () => {
   }
 
   return (
-    <div style={{ marginTop: "20%" }}>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input
-          className={darkMode ? "dark-mode-input" : "main-input"}
-          type="text"
-          {...searchUsers}
-          placeholder="Escribe el nombre y/o apellido"
-        />
-      </form>
-      <Table
-        style={{ fontFamily: "heeboregular", fontWeigth: 700 }}
-        className={darkMode ? "dark-mode" : "light"}
-        responsive
-        hover
-        size="sm"
-      >
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Nombre</th>
-            <th>Trabaja en</th>
-          </tr>
-        </thead>
-        <tbody>
-          {showedUsers[0] &&
-            showedUsers.map((user, i) => (
+    <div style={{marginTop:"20%"}}>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input
+            className={darkMode ? "dark-mode-input" : "main-input"}
+            type="text"
+            {...searchUsers}
+            placeholder="Escribe el nombre y/o apellido"
+          />
+        </form>
+        <Table
+          style={{ fontFamily: "heeboregular", fontWeigth: 700 }}
+          className={darkMode ? "dark-mode" : "light"}
+          responsive
+          hover
+          size="sm"
+        >
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Nombre</th>
+                <th>Trabaja en</th>
+              </tr>
+            </thead>
+          <tbody>
+            {showedUsers[0] && showedUsers.map((user, i) => (
               <tr key={i}>
                 <td>{i + 1}</td>
                 <td
@@ -92,25 +88,23 @@ const Admin = () => {
                   <td />
                 ) : (
                   <td>
-                    {user.admin ? (
-                      <FaUserLock
-                        style={{ cursor: "pointer" }}
-                        size={28}
-                        onClick={() => handleRemoveAdmin(user._id)}
-                      />
-                    ) : (
-                      <FaUser
-                        style={{ cursor: "pointer" }}
-                        size={28}
-                        onClick={() => handleSetAdmin(user._id)}
-                      />
-                    )}
+                    {user.admin ? 
+                    <FaUserLock
+                      style={{ cursor: "pointer" }}
+                      size={28}
+                      onClick={() => handleRemoveAdmin(user._id)}
+                    />: <FaUser 
+                      style={{ cursor: "pointer" }}
+                      size={28}
+                      onClick={() => handleSetAdmin(user._id)}
+                    />}
                   </td>
                 )}
               </tr>
             ))}
-        </tbody>
-      </Table>
+          </tbody>
+        </Table>
+      
       <div>
         <button
           className={darkMode ? "dark-mode-black-button" : "main-button-black"}
