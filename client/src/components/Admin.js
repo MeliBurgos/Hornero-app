73a2977ac8a35499177e61705ebdcfeef78f2c4b
Benjamin Becerra <benjabecerrab@gmail.com>
2022-07-14 15:07:10 -0300

    commit final

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Table from "react-bootstrap/Table";
-import { FaUserLock, FaUser } from "react-icons/fa";
+import { FaKey, FaUser } from "react-icons/fa";
 
 import ProfileModal from "./ProfileModal";
 import { promoteUserToAdmin, removeUserToAdmin, getAllUsers } from "../store/admin";
+import { getAllFutureReservations } from "../store/reservations";
 import useInput from "../hooks/useInput";
+
+import { PieChart, Pie, CartesianGrid, Tooltip, XAxis, YAxis } from 'recharts';
+
+
 
 const Admin = ({ setShow, setAddFriend }) => {
   const dispatch = useDispatch();
@@ -14,7 +19,9 @@ const Admin = ({ setShow, setAddFriend }) => {
   const allUsers = useSelector((state) => state.admin);
   const showedUsers = searchUsers.value.length >= 3 ? filteredUsers : allUsers;
   const darkMode = useSelector((state) => state.darkMode);
+  const reservations = useSelector((state) => state.reservations);
   const [profile, setProfile] = useState(false);
+  const [utilidad, setUtilidad] = useState('')
 
   const handleSetAdmin = (id) => {
     dispatch(promoteUserToAdmin(id)).then(() => {
@@ -28,8 +35,9 @@ const Admin = ({ setShow, setAddFriend }) => {
     });
   };
 
-  useEffect(()=> {
+  useEffect(() => {
     dispatch(getAllUsers())
+    dispatch(getAllFutureReservations())
   }, [])
 
   // buscar usuarios para agregar
@@ -38,37 +46,48 @@ const Admin = ({ setShow, setAddFriend }) => {
       let newUsersList = []
       let searchLowerCase = searchUsers.value.toLowerCase();
       allUsers.forEach((user) => {
-      let fullName = `${user.name} ${user.surname}`
+        let fullName = `${user.name} ${user.surname}`
         return fullName.toLowerCase().includes(searchLowerCase)
-        || 
-        user.mainOffice.toLowerCase().includes(searchLowerCase) 
-        ? newUsersList.push(user) : null}
+          ||
+          user.mainOffice.toLowerCase().includes(searchLowerCase)
+          ? newUsersList.push(user) : null
+      }
       )
       setFilteredUsers(newUsersList)
     }
   }, [searchUsers.value, allUsers]);
+ 
+
 
   if (profile) {
     return <ProfileModal profile={profile} setProfile={setProfile} />;
   }
 
   return (
-    <div style={{marginTop:"20%"}}>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input
-            className={darkMode ? "dark-mode-input" : "main-input"}
-            type="text"
-            {...searchUsers}
-            placeholder="Escribe el nombre y/o apellido"
-          />
-        </form>
-        <Table
-          style={{ fontFamily: "heeboregular", fontWeigth: 700 }}
-          className={darkMode ? "dark-mode" : "light"}
-          responsive
-          hover
-          size="sm"
-        >
+    <>
+      <div className="text-center" style={{ marginTop: "20%" }} >
+        <button onClick={() => setUtilidad('admin')} className='main-button mx-2'>Administrar Permisos</button>
+
+        <button onClick={() => setUtilidad("reportes")} className='main-button mx-2'>Reportes</button>
+      </div>
+
+      {utilidad === "admin" &&
+        <div className="p-4" >
+          <form onSubmit={(e) => e.preventDefault()}>
+            <input
+              className={darkMode ? "dark-mode-input" : "main-input"}
+              type="text"
+              {...searchUsers}
+              placeholder="Escribe el nombre y/o apellido"
+            />
+          </form>
+          <Table
+            style={{ fontFamily: "heeboregular", fontWeigth: 700 }}
+            className={darkMode ? "dark-mode mt-2" : "light mt-2"}
+            responsive
+            hover
+            size="sm"
+          >
             <thead>
               <tr>
                 <th>#</th>
@@ -76,53 +95,48 @@ const Admin = ({ setShow, setAddFriend }) => {
                 <th>Trabaja en</th>
               </tr>
             </thead>
-          <tbody>
-            {showedUsers[0] && showedUsers.map((user, i) => (
-              <tr key={i}>
-                <td>{i + 1}</td>
-                <td
-                  onClick={() => setProfile(user)}
-                >{`${user.name} ${user.surname}`}</td>
-                <td>{user.mainOffice}</td>
-                {showedUsers.includes(user._id) ? (
-                  <td />
-                ) : (
-                  <td>
-                    {user.admin ? 
-                    <FaUserLock
-                      style={{ cursor: "pointer" }}
-                      size={28}
-                      onClick={() => handleRemoveAdmin(user._id)}
-                    />: <FaUser 
-                      style={{ cursor: "pointer" }}
-                      size={28}
-                      onClick={() => handleSetAdmin(user._id)}
-                    />}
-                  </td>
-                )}
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      
-      <div>
-        <button
-          className={darkMode ? "dark-mode-black-button" : "main-button-black"}
-          onClick={() => setAddFriend(false)}
-        >
-          Volver
-        </button>
-        <button
-          className={darkMode ? "dark-mode-black-button" : "main-button-black"}
-          onClick={() => {
-            setShow(false);
-            setAddFriend(false);
-          }}
-        >
-          Cerrar
-        </button>
-      </div>
-    </div>
+            <tbody>
+              {showedUsers[0] && showedUsers.map((user, i) => (
+                <tr key={i}>
+                  <td>{i + 1}</td>
+                  <td
+                    onClick={() => setProfile(user)}
+                  >{`${user.name} ${user.surname}`}</td>
+                  <td>{user.mainOffice}</td>
+                  {showedUsers.includes(user._id) ? (
+                    <td />
+                  ) : (
+                    <td>
+                      {user.admin ?
+                        <FaKey
+                          style={{ cursor: "pointer" }}
+                          size={28}
+                          onClick={() => handleRemoveAdmin(user._id)}
+                        /> : <FaUser
+                          style={{ cursor: "pointer" }}
+                          size={28}
+                          onClick={() => handleSetAdmin(user._id)}
+                        />}
+                    </td>
+                  )}
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>}
+
+      {reservations && utilidad === "reportes" &&
+        <>
+          <h1>Total de reservas:</h1>
+
+          <PieChart width={730} height={250}>
+            <Pie data={[{"name": "Ocupado", "value": reservations.length},{"name": "Libre", "value": 500-reservations.length}]} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+          </PieChart>
+
+        </>
+      }
+
+    </>
   );
 };
 
--- client/src/components/ReserveAlert.js
+++ client/src/components/ReserveAlert.js
@@ -24,7 +24,7 @@ const ReserveAlert = ({
   const [showShareModal, setShowShareModal] = useState(false);
   const [profile, setProfile] = useState({})
 
-  const userAdmin = JSON.parse(localStorage.getItem("user"));
+ const userAdmin = JSON.parse(localStorage.getItem("user")).user.admin
 
   const handleRemoveFromFavorites = async (desk) => {
     await dispatch(removeFavorite(`${officeNameOk}:${desk}`));
@@ -144,7 +144,7 @@ const ReserveAlert = ({
           </button>
         ) : (
           ((Show.reserve[0].user._id === user._id ||
-          userAdmin.user.admin === true) && (
+          userAdmin) && (
             <div style={{ display: "flex", justifyContent: "space-around" }}>
               <button
                 className={"main-button"}
